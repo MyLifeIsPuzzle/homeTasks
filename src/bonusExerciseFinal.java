d99e37088e00d1c837186839c742cb7b419098cc
вашÐMyLifeIsPuzzle < mylifeispuzzle@gmail.com>
2018-02-05 22:30:44 +0300

    Bonus exercise final

--- src/bonusExerciseFinal.java
+++ src/bonusExerciseFinal.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class bonusEx {
+public class bonusExerciseFinal {
 
     public static void main(String[] args) {
         Map<Integer, Integer> someMap = new LinkedHashMap<>();
